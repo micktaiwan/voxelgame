d6194690357561d439228a346845179b07f8bd7c
Mickael Faivre-Macon <faivrem@gmail.com>
2013-12-07 12:55:14 +0100

    better management of active page for the header

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gameApp')
+        .controller('HeaderCtrl', function($scope, $location) {
+
+			$scope.isActive = function (viewLocation) {
+			        return viewLocation === $location.path();
+			    };
+
+        })
