57abf98988163f37d54e445cdf48e2177bd6a593
Mickael Faivre-Macon <faivrem@gmail.com>
2013-12-07 14:31:28 +0100

    fix

---
+++ app/scripts/controllers/users.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('gameApp')
+        .controller('UsersCtrl', function($scope, Db) {
+
+            var users = [];
+            Db.init();
+            //Db.addUser('Mickael', 'email mick');
+
+            Db.getUsers(function(users) {
+                $scope.users = []; // we reinitialize all users
+                for (var i in users) {
+                    $scope.users.push(Db.newUser(i, users[i].name, users[i].email));
+                }
+                ;
+                console.log($scope.users.length + ' users')
+            });
+            $scope.addUser = function(name) {
+                Db.addUser(name, 'no email');
+            };
+        });
